edf8f0a07696c641ed59f2940270c18244c75c9b
marvin guo <mwguo07@gmail.com>
2021-03-03 01:41:35 +0800

    linked list

--- 700_799/700_799_test.go
+++ 700_799/700_799_test.go
@@ -41,3 +41,16 @@ func TestMyHashMap(t *testing.T) {
 	m.Remove(2)
 	fmt.Println(m.Get(2))
 }
+
+func TestRemoveElements(t *testing.T) {
+	l1 := &ListNode{Val: 1}
+	l2 := &ListNode{Val: 2}
+	l3 := &ListNode{Val: 3}
+	l4 := &ListNode{Val: 2}
+	l5 := &ListNode{Val: 5}
+	l1.Next = l2
+	l2.Next = l3
+	l3.Next = l4
+	l4.Next = l5
+	fmt.Println(splitListToParts(l1, 3))
+}
